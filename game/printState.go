522afc5f060eaf5ca1cf3ce0ae244d5eccce8386
Chopikashvili <chopikashvilid@gmail.com>
2023-09-11 11:49:43 +0300

    Remake game display

--- game/printState.go
+++ game/printState.go
@@ -6,22 +6,30 @@ import (
 )
 
 func PrintState(deal Deal) {
-	fmt.Print("Community cards:")
+	fmt.Print("Community cards: ")
 	for _, c := range deal.community {
 		fmt.Print(c.Read())
+		fmt.Print(" ")
 	}
 	fmt.Println(" ")
+	human := deal.players[slices.IndexFunc(deal.players, func(p Player) bool { return p.id == deal.humanPlayerId })]
+	humanHand1, humanHand2 := ReadHand(human)
+	fmt.Printf("Your hand: %s %s", humanHand1, humanHand2)
 	fmt.Println(" ")
 	fmt.Printf("Pot: %d", deal.pot)
 	fmt.Println(" ")
 	fmt.Println(" ")
-	for _, p := range deal.players {
-		fmt.Println(p.Name)
+	for i, p := range deal.players {
+		fmt.Print(p.Name)
+		if i == deal.dealerId {
+			fmt.Print(", dealer")
+		}
+		if p.HasFolded {
+			fmt.Print(", folded")
+		}
+		fmt.Println(" ")
 		fmt.Printf("%d chips, %d bet", p.Chips, p.Bet)
 		fmt.Println(" ")
 	}
-	human := deal.players[slices.IndexFunc(deal.players, func(p Player) bool { return p.id == deal.humanPlayerId })]
-	humanHand1, humanHand2 := ReadHand(human)
-	fmt.Printf("Your hand: %s %s", humanHand1, humanHand2)
 	fmt.Println(" ")
 }
